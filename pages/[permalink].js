3f49bc374147da6c482b03a2cf1bc056102d8b71
Kognise <felix.mattick@gmail.com>
2019-09-05 11:01:47 -0400

    Add deck

--- pages/[permalink].js
+++ pages/[permalink].js
@@ -5,6 +5,7 @@ import ArticleBody from '../components/ArticleBody'
 
 const IndexPage = (props) => (<>
   <h1 dangerouslySetInnerHTML={{ __html: props.article.title }} />
+  {props.article.deck ? <h2 dangerouslySetInnerHTML={{ __html: props.article.deck }} /> : null}
   <ArticleBody ast={props.parsed} />
 </>)
 
