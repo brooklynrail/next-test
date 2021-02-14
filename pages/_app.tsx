0381b08254efc686c58ef541ec2e3eeec03bb1bc
Jeremy Zilar <jeremyzilar@gmail.com>
2021-02-14 15:52:54 -0700

    change files to tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import App from 'next/app'
+import App, { AppProps } from 'next/app'
+import Head from "next/head"
 import fonts from '../styles/fonts'
 
 export default class extends App {
   render() {
     const { Component, pageProps } = this.props
     return (<>
-      <style jsx global>{fonts}</style>
+      <Head>
+        <style jsx global>{fonts}</style>
       <style jsx global>{`
         body {
           font-family: 'Untitled Sans', sans-serif;
           font-size: 1.125em;
         }
       `}</style>
-
+      </Head>
       <Component {...pageProps} />
     </>)
   }
